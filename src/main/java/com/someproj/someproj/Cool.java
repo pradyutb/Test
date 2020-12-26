829b1f0fb826a846f0f782a9fc109461f037f4f4
Pradyut Bhattacharya <pradiut@yahoo.com>
2020-12-27 02:02:32 +0530

    Initial Commit

---
+++ src/main/java/com/someproj/someproj/Cool.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.someproj.someproj;
+
+/**
+ *
+ * @author Pradyut
+ */
+public class Cool {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Testing");
+        
+    }
+    
+}
